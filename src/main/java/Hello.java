3b9fcfea672abe07f93081d4ecb13e7066aa77b9
User <vincent20030808@gmail.com>
2022-03-04 16:13:53 +0800

    initial commit

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,6 +1,17 @@
 
 public class Hello {
     public static void main(String[] args ){
-        System.out.println("Hello");
+        String[]names={"A","B","C"};
+        int[] math={60,50,70};
+        int[] english={75,65,50};
+        for (int i=0;i<3;i++){
+            System.out.print(names[i]+" ");
+            System.out.print(math[i]+" ");
+            System.out.print(english[i]+" ");
+            System.out.println();
+        }
+
+
+
     }
 }
---
+++ src/main/java/Scoring.java
@@ -0,0 +1,8 @@
+public class Scoring {
+    public static void main(String[] args ){
+        Student jack=new Student();
+        jack.math=65;
+        jack.english=80;
+        jack.name="jack";
+    }
+}
